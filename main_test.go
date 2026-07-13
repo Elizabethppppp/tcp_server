35b207e1c2fe25b2cde5252e6756825bb004956d
Elizabeth <pirogelizaveta237@gmail.com>
2026-07-13 14:41:21 +0300

    переделанный тест

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestParseRequestLine(t *testing.T) {
 	t.Parallel()
@@ -78,6 +81,34 @@ func TestParseRequestLine(t *testing.T) {
 	}
 }
 
+// test for handler
+type trainResponseWriter struct {
+	status  int
+	headers map[string]string
+	body    *bytes.Buffer
+}
+
+func newTrainResponseWriter() *trainResponseWriter {
+	return &trainResponseWriter{
+		headers: make(map[string]string),
+		body:    &bytes.Buffer{},
+	}
+}
+
+func (tr *trainResponseWriter) WriteHeader(status int) {
+	tr.status = status
+}
+
+func (tr *trainResponseWriter) SetHeader(key, value string) {
+	tr.headers[key] = value
+}
+
+func (tr *trainResponseWriter) Write(body []byte) (int, error) {
+	// tr.body = append(tr.body, body...)
+	// return len(body), nil
+	return tr.body.Write(body)
+}
+
 func TestHandleHello(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
@@ -86,9 +117,9 @@ func TestHandleHello(t *testing.T) {
 		wantBody string
 	}{
 		{
-			name:     "есть путь",
-			path:     "/hello",
-			wantBody: "Hello, /hello!",
+			name:     "путь есть",
+			path:     "/world",
+			wantBody: "Hello, /world!",
 		},
 		{
 			name:     "пустой путь",
@@ -104,21 +135,19 @@ func TestHandleHello(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			w := &responseWriter{
-				Headers: make(map[string]string),
-			}
-			r := &Request{
-				Path: tt.path,
-			}
+			w := newTrainResponseWriter()
+			r := &Request{Path: tt.path}
+
 			handleHello(w, r)
-			if w.Status != 200 {
-				t.Errorf("Status: got %d, want 200", w.Status)
+
+			if w.status != 200 {
+				t.Errorf("status: got %d, want 200", w.status)
 			}
-			if string(w.Body) != tt.wantBody {
-				t.Errorf("Body: got %q, want %q", string(w.Body), tt.wantBody)
+			if w.body.String() != tt.wantBody {
+				t.Errorf("body: got %q, want %q", w.body.String(), tt.wantBody)
 			}
-			if w.Headers["Content-Type"] != "text/plain" {
-				t.Errorf("Content-Type: got %q, want %q", w.Headers["Content-Type"], "text/plain")
+			if w.headers["Content-Type"] != "text/plain" {
+				t.Errorf("Content-Type: got %q, want %q", w.headers["Content-Type"], "text/plain")
 			}
 		})
 	}
